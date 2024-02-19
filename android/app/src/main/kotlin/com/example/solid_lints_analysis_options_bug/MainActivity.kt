d2f24874eadec953d3b5f152c29306a027aaa900
danylo-safonov-solid <danylo.safonov+braav@solid.software>
2024-02-19 23:33:29 +0200

    repro

---
+++ android/app/src/main/kotlin/com/example/solid_lints_analysis_options_bug/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.solid_lints_analysis_options_bug
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/x.dart
@@ -0,0 +1,7 @@
+// ignore_for_file: no_leading_underscores_for_local_identifiers, non_constant_identifier_names, lines_longer_than_80_chars, avoid_unused_parameters
+
+class X {
+  static int x(int _1, int _2, int _3, int _4, int _5, int _6, int _7, int _8) {
+    return 10;
+  }
+}
